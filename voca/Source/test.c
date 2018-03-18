a3aaabd1cb424ac787db0507f71099c581c07643
jesucristo <brianjerezb@gmail.com>
2018-03-18 00:18:24 -0500

    added a few things

--- voca/Source/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "voca.h"
-
-int main(void)
-{
-	printf("%d\n", LastIndexOf("we have a mission", "mission"));
-	printf("%d\n", LastIndexOfSingleChar("we have a mission", 'a'));
-	return 0;
-}
